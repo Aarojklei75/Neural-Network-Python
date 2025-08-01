94a57e7a65d0d43f2e2499892dc6e1c44f5f4367
Aaron <Aaron.Klein051@gmail.com>
2025-07-31 22:42:10 -0500

    Initial commit

---
+++ .gitignore/__init__.py
@@ -0,0 +1,24 @@
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*$py.class
+
+# Data files
+*.pickle
+mnist.pickle
+
+# Virtual environment
+.venv/
+env/
+venv/
+
+# Jupyter Notebook checkpoints
+.ipynb_checkpoints
+
+# IDEs
+.vscode/
+.idea/
+
+# OS junk
+.DS_Store
+Thumbs.db
